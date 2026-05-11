5d1ab5ea404d36bd603810cf2f62572fd40b15a0
Cheese3008 <your_email@example.com>
2026-05-11 18:18:11 +0700

    fix: Correct header file extension for cv_bridge inclusion

--- src/point_mission_mode/PointMissionMode.cpp
+++ src/point_mission_mode/PointMissionMode.cpp
@@ -11,7 +11,7 @@
 #include <sstream>
 #include <stdexcept>
 
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/imgproc.hpp>
 #include <sensor_msgs/image_encodings.hpp>
 
